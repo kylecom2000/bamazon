97f054ab28aee7b8b65461fff3235788c09b4a6e
FightingSprrow <kbauertx@gmail.com>
2018-11-28 09:49:09 -0600

    finished database entries, connection to server working

--- bamazon.sql
+++ bamazon.sql
@@ -5,21 +5,27 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+  item_id INT(11) NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price INT NULL,
+  stock_quantity INT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Adjustable Laptop Stand", "Computer Accessories", 80, 10),
+("Bose QuietComfort 35", "Electronics", 300, 50),
+("Gold Fish", "Groceries", 5, 500),
+("Mac Book Pro", "Electronics", 2500, 3),
+("Clif Bars", "Groceries", 5, 250),
+("Bamazon Echo Silver", "Electronics", 30, 10000),
+("Xmas Sweater", "Clothing", 20, 100),
+("FitBit", "Electronics", 250, 250),
+("RayBan Sunglasses", "Clothing Accesories", 193, 100),
+("xBox One", "Electronics", 250, 300);
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
+SELECT * FROM products
 
 -- ### Alternative way to insert more than one row
 -- INSERT INTO products (flavor, price, quantity)
---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 8889,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.query("SELECT * FROM products;", function(err, res){
+    if(err) throw err;
+    for(var i =0; i < res.length; i++){
+      console.log(`
+      ${res[i].product_name} $${res[i].price}
+      `);
+    }
+    
+  });
+  connection.end();
+});
